1a3a84769c2ea06177624c0706449a4c1f06fb97
anthonyanup21 <anthonyanup21@gmail.com>
2025-11-08 23:02:35 +0530

    day-1

---
+++ day-1/index.js
@@ -0,0 +1,56 @@
+let a = "20";
+console.log(+a);//20
+
+let b = "50";
+console.log(-b);//-50
+
+let c = true;
+console.log(+c);//1
+
+let d = false;
+console.log(+d);//0
+
+let e = "";
+console.log(+e);//0
+
+let f = "123abc";
+console.log(+f);//NaN
+
+let x = "10";
+let y = 5;
+console.log(+x + y);//15
+
+let p = "100";
+let q = "20";
+console.log(+p + +q);//120
+
+let k = "7";
+console.log(-(-k));//7
+
+let m = "   15   ";
+console.log(+m);//15
+
+console.log(+"0" + +"0" - -"1");//1
+
+let z = "42";
+console.log(-z + 10);//-32
+
+let w = "5.5";
+console.log(+w * 2);//11
+
+let n = " -9 ";
+console.log(+n);//-9
+
+let v="20"
+v+=10
+console.log(v)//2010
+
+
+
+
+
+
+
+
+
+
